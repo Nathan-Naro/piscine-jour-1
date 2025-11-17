fbfef74c541563a23d56360a3fa47b89cb618387
Nathan-naro <thoumieux.nathan@gmail.com>
2025-11-17 17:00:28 +0100

    lun. 17 nov. 2025 17:00:28 CET

---
+++ ex_05.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git config --global user.name
+git config --global user.email
+
+
+ 
+ git add README.md
+
+ git commit -m "Ex_05"
+
+ git push
+
+# tu tape ton email dans les guillemts EMAIL EPITECH
+# ensuite sur tape sur entrer
+# puis entrer
+# puis entrer
+# et tu tapes la commande ls -al ~/.ssh
---
+++ ex_06.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global log
+
+ git add log.txt
+
+ git commit -m "Ex_06"
+
+ git push
